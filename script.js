880aad3a81234ecbd9db51c1f5c6f04aff8ba78c
rodrigoespigares <rodrigoespigaresfernandez@gmail.com>
2023-10-25 09:39:25 +0200

    generacion

--- script.js
+++ script.js
@@ -1,9 +1,53 @@
-function number(){
-    aleatoryNumber = Math.floor(Math.random()*255);
-    return aleatoryNumber;
+function allColors(){
+    r = Math.floor(Math.random()*255);
+    g = Math.floor(Math.random()*255);
+    b = Math.floor(Math.random()*255);
+    return `rgb(${r},${g},${b})`;
+}
+
+function win(){
+    winnerColor = allColors();
+    document.getElementById("aleatorio").innerHTML = winnerColor;
+    return winnerColor;
 }
 
 function dificult(){
-    easy = document.getElementsById("easy");
-    hard = document.getElementById("hard");
+    let containers = document.querySelectorAll(".colores");
+    let easyBtn = document.getElementById("easy");
+    let hardBtn = document.getElementById("hard");
+    
+    easyBtn.addEventListener("click",() => {
+        let i=0;
+        for (const container of containers) {
+            if(i>=3){
+                
+                container.className = "oculto";
+            }
+            i++;
+        }
+    })
+    hardBtn.addEventListener("click",() => {
+        for (const container of containers) {
+            container.className = "colores";
+        }
+    })
+}
+let clic = false;
+function aleatorio(){
+    
+    let newColor = document.getElementById("new");
+    newColor.addEventListener("click", () => {
+        let winnerColor = win();
+        let containers = document.querySelectorAll(".colores");
+        let i=0;
+        let ganador = Math.floor(Math.random()*containers.length);
+        for (const container of containers) {
+            if(i==ganador){
+                container.style.backgroundColor = winnerColor;
+            }else{
+            container.style.backgroundColor = allColors();
+            }
+            i++;
+        }
+    })
 }
