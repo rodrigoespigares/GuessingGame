fb834205d89dc7b843b61bd9ad815ee85382ee39
rodrigoespigares <rodrigoespigaresfernandez@gmail.com>
2023-10-25 08:41:15 +0200

    start game

---
+++ script.js
@@ -0,0 +1,9 @@
+function number(){
+    aleatoryNumber = Math.floor(Math.random()*255);
+    return aleatoryNumber;
+}
+
+function dificult(){
+    easy = document.getElementsById("easy");
+    hard = document.getElementById("hard");
+}
